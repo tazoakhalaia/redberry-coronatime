f4c45d6205ca295454c644c4e799e43a7cd37e84
tazoakhalaia <darknemiroff@gmail.com>
2023-05-12 14:00:58 +0400

    done header markup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,35 +8,41 @@
     <title>Document</title>
 </head>
 <body>
-    <header class="w-full bg-red-600 h-20">
-        <div class="bg-blue-500 m-auto w-10/12 h-full">
+    <header class="w-full h-20">
+        <div class="m-auto w-10/12 h-full flex justify-between items-center">
             <div>
                 <img src="{{ asset('images/coronatime.svg') }}">
             </div>
-            <div>
-                <select>
+            <div class="userbox flex">
+                <select class="mr-10 bg-transparent outline-none">
                     <option value="english">English</option>
                     <option value="georgian">Georgian</option>
                 </select>
-                <div>
+                <div class="mr-10 capitalize">
                     <h1>username</h1>
                 </div>
                 <form action="{{ route('logout') }}" method="post">
                     @csrf
-                    <button class="bg-blue-600">logout</button>
+                    <button class="bg-tranparent capitalize">log out</button>
                 </form>
             </div>
         </div>
     </header>
-    <button class="worldwidebtn">worldwide</button>
-    <button class="countrybtn">country</button>
-    <div class="worldwide w-full bg-red-500 mt-10">
-        asdsad
+    <main class="w-10/12 m-auto">
+    <h1 class="font-bold text-md">Worldwide Statistics</h1>
+    <div class="mt-10">
+    <button class="worldwidebtn">Worldwide</button>
+    <button class="countrybtn ml-10">By country</button>
+    </div>
+    <div class="worldwide w-full mt-10">
+        <div class="newcases w-96 h-60 bg-blue-300 rounded-md">
+            sad
+        </div>
     </div>
     <div class="country hidden w-full bg-blue-500 mt-10">
         asdsad
     </div>
-
+    </main>
 
     <!---->
     <script>
@@ -47,11 +53,15 @@
         worldWideBtn.addEventListener('click', ()=> {
             country.style.display = "none";
             worldWide.style.display = "flex";
+            worldWideBtn.style.fontWeight = "500"
+            countrybtn.style.fontWeight = "300"
         });
 
         countrybtn.addEventListener('click', ()=> {
             worldWide.style.display = "none";
             country.style.display = "flex";
+            countrybtn.style.fontWeight = "500"
+            worldWideBtn.style.fontWeight = "300"
         });
     </script>
 </body>
