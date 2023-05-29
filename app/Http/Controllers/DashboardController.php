292f9d7df0405e09c861e4c564bb9b516a60295b
tazoakhalaia <darknemiroff@gmail.com>
2023-05-29 21:10:10 +0400

    fix code

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -9,17 +9,12 @@ use Illuminate\View\View;
 class DashboardController extends Controller
 {
     public function index(Request $request): View
-    {
-        $countries = Country::all();
-        if ($request->input('sort') == 'location') {
-            $countries = $countries->sortBy('name');
-        } elseif ($request->input('sort') == 'recovered') {
-            $countries = $countries->sortByDesc('recovered', SORT_NUMERIC);
-        } elseif ($request->input('sort') == 'deaths') {
-            $countries = $countries->sortByDesc('deaths', SORT_NUMERIC);
-        } elseif ($request->input('sort') == 'confirmed') {
-            $countries = $countries->sortByDesc('confirmed', SORT_NUMERIC);
-        }
-        return view('dashboard', ['countries' => $countries, 'user' => auth()->user()]);
-    }
+{
+    $sort = $request->input('sort');
+    $direction = $request->input('direction', 'asc'); 
+    $countries = Country::sortByField($sort, $direction)->get();
+    
+    return view('dashboard', ['countries' => $countries, 'user' => auth()->user()]);
+}
+
 }
--- app/Models/Country.php
+++ app/Models/Country.php
@@ -9,4 +9,14 @@ class Country extends Model
 {
     use HasFactory;
     protected $fillable = ['name'];
+    public function scopeSortByField($query, $field, $direction = 'asc')
+{
+    $sortableFields = ['name', 'recovered', 'deaths', 'confirmed'];
+    if (!in_array($field, $sortableFields)) {
+        return $query;
+    }
+    
+    return $query->orderBy($field, $direction);
+}
+
 }
